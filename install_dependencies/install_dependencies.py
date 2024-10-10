25f9fcf21c1734679bedde1a04ddbda65a9ecfdb
Daniel-W-Blender-Python <142774885+Daniel-W-Blender-Python@users.noreply.github.com>
2024-10-09 20:49:22 -0700

    Create install_dependencies.py

---
+++ install_dependencies/install_dependencies.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import subprocesses
+
+python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+py_lib = os.path.join(sys.prefix, 'lib', 'site-packages','pip')
+subprocess.check_call([sys.executable, "-m", "ensurepip"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "tensorflow"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jaxlib"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jax"])
