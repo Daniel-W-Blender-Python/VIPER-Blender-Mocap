626ce58428bafb209bd7f2a71fc387587d2a5cbc
Daniel-W-Blender-Python <142774885+Daniel-W-Blender-Python@users.noreply.github.com>
2024-11-03 10:45:26 -0700

    Update install_dependencies.py

--- install_dependencies/install_dependencies.py
+++ install_dependencies/install_dependencies.py
@@ -5,6 +5,7 @@ import subprocess
 python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
 py_lib = os.path.join(sys.prefix, 'lib', 'site-packages','pip')
 subprocess.check_call([sys.executable, "-m", "ensurepip"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "opencv-python"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "tensorflow"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jaxlib"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "jax"])
